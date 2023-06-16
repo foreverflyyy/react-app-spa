3e41e69fbaa26f78c3ecb6f4f45a52d1f4f0154a
Nikita Ushakov <nikita.ushackow.02@gmail.com>
2023-06-16 15:53:09 +0200

    dev: add hiding when press on links in navbar
    dev: add hiding when press on links in navbar

--- src/UI/Header.tsx
+++ src/UI/Header.tsx
@@ -1,36 +1,22 @@
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import {Link} from "react-router-dom";
-import {Image} from "react-bootstrap";
+import SectionOffcanvas from "./SectionOffcanvas";
+import {useState} from "react";
 
 function Header() {
+
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
         <>
             <Navbar collapseOnSelect expand="xxl" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/posts">Me React App</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Offcanvas
-                        id={`nameNavbar-expand-xxl`}
-                        aria-labelledby={`nameNavbarLabel-expand-xxl`}
-                        placement="start"
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Image src="./myAvatar.png" rounded fluid />
-                            <Offcanvas.Title id={`nameNavbarLabel-expand-xxl`}>
-                                Nikita
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Link to="/about">nikita.ushakov.02@outlook.com</Link>
-                                <Link to="/posts">List of Posts</Link>
-                                <Link to="/about">About me</Link>
-                            </Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
+                    <Navbar.Brand>My React App</Navbar.Brand>
+                    <Navbar.Toggle
+                        aria-controls="responsive-navbar-nav"
+                        onClick={() => setShowMenu(!showMenu)}
+                    />
+                    <SectionOffcanvas showMenu={showMenu} changeShowMenu={setShowMenu}/>
                 </Container>
             </Navbar>
         </>
---
+++ src/UI/SectionOffcanvas.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from "react-bootstrap/Image";
+import Nav from "react-bootstrap/Nav";
+import {Link} from "react-router-dom";
+import {Offcanvas} from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+
+interface SectionOffcanvasProps {
+    showMenu: boolean
+    changeShowMenu: (val: boolean) => void;
+}
+
+const SectionOffcanvas = ({showMenu, changeShowMenu}: SectionOffcanvasProps) => {
+    const toggleMenu = () => {
+        changeShowMenu(!showMenu)
+    }
+
+    const handleClose = () => changeShowMenu(false)
+
+    return (
+        <Navbar.Offcanvas
+            id={`nameNavbar-expand-xxl`}
+            aria-labelledby={`nameNavbarLabel-expand-xxl`}
+            placement="start"
+            show={showMenu}
+            onHide={handleClose}
+        >
+            <Offcanvas.Header closeButton>
+                <Image src="./myAvatar.png" rounded fluid />
+                <Offcanvas.Title id={`nameNavbarLabel-expand-xxl`}>
+                    Nikita
+                </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Nav onClick={handleClose} className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link as={Link} onClick={toggleMenu} to={'/about'}>nikita.ushakov.02@outlook.com</Nav.Link>
+                    <Nav.Link as={Link} onClick={toggleMenu} to={'/posts'}>List of Posts</Nav.Link>
+                    <Nav.Link as={Link} onClick={toggleMenu} to={'/about'}>About me</Nav.Link>
+                </Nav>
+            </Offcanvas.Body>
+        </Navbar.Offcanvas>
+    );
+};
+
+export default SectionOffcanvas;
